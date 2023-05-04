4f1fbcc3b3a12df91006de4abc65d64df5f6879f
hebrohim <raheemibrohim@mail.com>
2023-05-05 00:13:15 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+    
+  return (
+    <div className=' bg-blue-500'>
+
+<section className='mb-5'>this is the menu section</section>
+<section className='w-screen h-[40vh] px-10' >
+    <input type="text" className='w-full px-2 text-blue-500 font-semibold border-none outline-none h-10 rounded-md shadow-md shadow-black'
+    
+    />
+
+
+<h1></h1>
+</section>
+
+    </div>
+  )
+}
+
+export default Header
